6784065822f39888e863a6dcf4503ef4b219df85
Harry Cunningham <147438079+harrycunningham04@users.noreply.github.com>
2025-01-23 16:11:19 +0000

    Add files via upload
    the addition of the empty state that shows on the dashboard if the user has no events scheduled with a button leading them to the new event

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,46 @@
+import { notFound } from "next/navigation";
+import prisma from "../lib/db";
 import { requireUser } from "../lib/hooks";
+import { EmptyState } from "../components/EmptyState";
+
+async function getData(userId: string) {
+  const data = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      userName: true,
+      eventType:{
+        select:{
+          id:true,
+          active:true,
+          title:true,
+          url:true,
+          duration:true,
+        }
+      }
+    }
+  });
+
+  if (!data) {
+    return notFound();
+  }
+
+  return data;
+}
 
 export default async function Dashboard() {
 const session = await requireUser();
 
+const data = await getData(session.user?.id as string);
+
   return (
-    <div>
-      <h1>Hi from the Dashboard</h1>
-    </div>
+    <>
+      {data.eventType.length === 0 ? (
+        <EmptyState title="No Event Types" description="You can create your first event by clicking the button below" buttonText="Create Event" href="/dashboard/new" />
+        ) : (
+          <p>atleast 1 event</p>
+        )}
+    </>
   );
 }
