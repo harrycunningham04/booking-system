153ae74cde81f0e97379ccc3218ea5e618e3fafa
Harry Cunningham <147438079+harrycunningham04@users.noreply.github.com>
2025-01-26 14:30:23 +0000

    Add files via upload
    updated version

--- app/(bookingPage)/[userName]/[eventUrl]/page.tsx
+++ app/(bookingPage)/[userName]/[eventUrl]/page.tsx
@@ -45,15 +45,27 @@ async function getData(userName: string, eventUrl: string) {
 
 const BookingPage = async ({
   params,
+  searchParams,
 }: {
   params: { userName: string; eventUrl: string };
+  searchParams: { date?: string };
 }) => {
   const data = await getData(params.userName, params.eventUrl);
+
+  const selectedDate = searchParams.date
+    ? new Date(searchParams.date)
+    : new Date();
+
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    weekday: "short",
+    day: "numeric",
+    month: "long",
+  }).format(selectedDate);
 
   return (
     <div className="min-h-screen w-screen flex items-center justify-center">
       <Card className="max-w-[1000px] w-full mx-auto">
-        <CardContent className="p-5 md:grid md:grid-cols-[1fr,auto,1fr,auto,1fr]">
+        <CardContent className="p-5 md:grid md:grid-cols-[1fr,auto,1fr,auto,1fr] gap-4">
           <div>
             <img
               src={data.User?.image as string}
@@ -69,28 +81,36 @@ const BookingPage = async ({
             </p>
 
             <div className="mt-5 flex flex-col gap-y-3">
-
               <p className="flex items-center">
                 <CalendarX2 className="size-4 mr-2 text-primary" />
-                <span className="text-sm font-medium text-muted-foreground">25. Jan 2025</span>
+                <span className="text-sm font-medium text-muted-foreground">
+                  {formattedDate}
+                </span>
               </p>
 
               <p className="flex items-center">
                 <Clock className="size-4 mr-2 text-primary" />
-                <span className="text-sm font-medium text-muted-foreground">{data.duration} Mins</span>
+                <span className="text-sm font-medium text-muted-foreground">
+                  {data.duration} Mins
+                </span>
               </p>
 
               <p className="flex items-center">
                 <VideoIcon className="size-4 mr-2 text-primary" />
-                <span className="text-sm font-medium text-muted-foreground">{data.videoCallSoftware}</span>
+                <span className="text-sm font-medium text-muted-foreground">
+                  {data.videoCallSoftware}
+                </span>
               </p>
-
             </div>
           </div>
 
           <Separator orientation="vertical" className="h-full w-[1px]" />
 
-          <RenderCalendar />
+          <RenderCalendar availability={data.User?.availability as any} />
+
+          <Separator orientation="vertical" className="h-full w-[1px]" />
+
+          
         </CardContent>
       </Card>
     </div>
