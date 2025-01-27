5e768b06c964a6ac2e104a3aac598c89c88bd47c
Harry Cunningham <147438079+harrycunningham04@users.noreply.github.com>
2025-01-27 00:26:44 +0000

    Add files via upload
    adding the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
+import { Hero } from "./components/Hero";
 import { NavBar } from "./components/NavBar";
 import { auth } from "./lib/auth";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-
   const session = await auth();
 
-  if(session?.user){
+  if (session?.user) {
     return redirect("/dashboard");
   }
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <NavBar />
+      <Hero />
     </div>
   );
 }
